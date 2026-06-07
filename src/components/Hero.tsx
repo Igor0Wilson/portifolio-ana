bc9566fe8144e77bb0878405fecf6ec890834a45
Igor Silva <igorwilsonsimiao@gmail.com>
2026-06-06 21:42:15 -0300

    ajuste de imagens

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import profileImg from '../assets/ana_flavia.png';
 
 const TYPED_ROLES = ['Cuidados Críticos', 'Nefrologia e Hemodiálise', 'Assistência Domiciliar', 'Saúde Pública'];
